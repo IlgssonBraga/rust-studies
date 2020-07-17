2c3a58bbc43d02ab814d65319ff8a2984b4c59c8
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-17 16:10:10 -0300

    feat: handling crates 🦀⚙

---
+++ crates/src/main.rs
@@ -0,0 +1,10 @@
+extern crate time;
+
+fn main() {
+    let d = time::now();
+    println!("Today is {}/{}/{}", d.tm_mday, d.tm_mon, d.tm_year + 1900);
+}
+
+// fn main() {
+//     println!("Hello world");
+// }
