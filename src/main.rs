8e14079c11fbf18893e935deeef1ef4ec36756fc
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-17 13:04:39 -0300

    feat: std::io 🦀⚙

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let greeting = "Hello";
+    let mut name = String::new();
+
+    io::stdin().read_line(&mut name);
+
+    println!("{}, {}!", greeting, name);
 }
