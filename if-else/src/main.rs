715ab159ea995ffce7fe2182c453587e2f35c2ec
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-18 11:32:45 -0300

    feat: multiples if 🦀⚙

--- if-else/src/main.rs
+++ if-else/src/main.rs
@@ -1,3 +1,25 @@
+fn check_sit(nota: f32) {
+    if nota < 3.0 {
+        println!("Reprovado direto");
+    } else if nota >= 3.0 && nota < 7.0 {
+        println!("Avaliação final");
+    } else if nota >= 7.0 && nota < 10.0 {
+        println!("Aprovado por média");
+    } else if nota == 10.0 {
+        println!("Aprovado com excelência");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let nota: f32 = 10.0;
+
+    check_sit(nota);
+
+    // let x = if 10 + 5 == 15 {
+    //     "10 + 5 = 15"
+    // } else {
+    //     "10 + 5 != 15"
+    // };
+
+    // println!("{}", x);
 }
