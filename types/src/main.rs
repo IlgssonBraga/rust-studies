081a4377121bc35383919dd5461622742913a1b8
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-18 11:20:05 -0300

    feat: modules 🦀⚙

--- types/src/main.rs
+++ types/src/main.rs
@@ -3,7 +3,23 @@
 // mod numeric_values;
 // mod arrays_slices;
 // mod tuples;
-mod enums;
+// mod enums;
+
+mod test {
+
+    // private module
+    fn message() {
+        println!("Show message");
+    }
+
+    // public module
+    pub fn sum(a: i32, b: i32) -> i32 {
+        a + b
+    }
+}
+
+// nomeia um módulo
+use test::sum as sum2;
 
 fn main() {
     // chars::run();
@@ -11,5 +27,10 @@ fn main() {
     // numeric_values::run();
     // arrays_slices::run();
     // tuples::run();
-    enums::run();
+    // enums::run();
+
+    println!("soma : {}", sum2(2, 3));
+
+    // Não conseguimos acessar o módulo message
+    // pq ele não é público
 }
