3643f6958b1ec3b7d5af8a0d2296e4feab9b7f9b
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-18 12:27:42 -0300

    feat: repetition-structures 🦀⚙

--- repetition-structures/src/main.rs
+++ repetition-structures/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    // let mut a = 1;
+
+    // while a <= 10 {
+    //     println!("{}", a);
+    //     a += 1;
+    // }
+
+    // loop {
+    //     println!("loop");
+    //     a += 1;
+
+    //     if a == 20 {
+    //         break;
+    //     }
+    // }
+
+    for i in 1..=10 {
+        // ..exclusive; ..= inclusive
+        println!("{}", i);
+    }
 }
